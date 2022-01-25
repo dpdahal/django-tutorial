b9c3d98702c4a28e69b0855359d4ad5b860fb2a6
dpdahal <dp48dahal@gmail.com>
2022-01-25 13:27:24 +0545

    update

--- tutorials/blog/urls.py
+++ tutorials/blog/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 from . import views
+from . import urlcheck
+
+register_converter(urlcheck.UrlCheck, 'newId')
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('about', views.about),
+    path('contact', views.contact),
+    path('news/<newId:id>', views.news)
+    # re_path(r'^news/(?P<id>[0-5]{3})/$', views.news),
+
 ]
--- tutorials/blog/views.py
+++ tutorials/blog/views.py
@@ -4,4 +4,18 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
+    print(dir(HttpResponse))
     return HttpResponse("<h1>Welcome blog section</h1>")
+
+
+def about(request):
+    return HttpResponse("About page")
+
+
+def contact(request):
+    return HttpResponse("Contact page")
+
+
+def news(request, id):
+    print(id)
+    return HttpResponse("news page")
